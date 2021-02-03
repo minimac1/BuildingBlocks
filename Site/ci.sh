0542412a1072805c547c62f2147243bce20d91cc
minimac1 <mini.mac@live.com>
2021-02-04 00:32:28 +1100

    adding more utility files

---
+++ Site/ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# maybe do some logic to work out path so i can move around without breaking
+if [[ -f "/user.env" ]]; then
+    echo "Set your user variables first in a user.env file"
+    exit 1
+fi
+
+set -a
+echo "Extracting config to ENV from user.env"
+source user.env
+set +a
+
+script_path="scripts/${1}.sh"
+exec "${script_path}"
